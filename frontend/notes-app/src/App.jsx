7aa1d5e2a447f103e860b066fb51e96f79b0471f
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-23 11:39:38 +0530

    Added React Router DOM and set up initial routing

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import Home from './pages/Home/Home'
+//import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+
+const routes = (
+  <Router>
+    <Routes>
+    <Route path="/dashboard" exact element={<Home />} />
+    <Route path="/login" exact element={<Login />} />
+    <Route path="/signup" exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <div>{routes}</div>
   )
 }
 
-export default App
+export default App;
