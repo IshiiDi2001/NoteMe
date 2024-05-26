b5b8a9ba15893850f653fdcbc9957b3cbd531806
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-26 22:44:26 +0530

    SignUp UI Added

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Passwordinput from "../../components/Input/Passwordinput";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default SignUp
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError("");
+
+    // SignUp API Call
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border border-gray-300 rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Passwordinput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Create Account
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Already have an account?{" "}
+              <Link to="/login" className="font-medium text-primary underline">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
