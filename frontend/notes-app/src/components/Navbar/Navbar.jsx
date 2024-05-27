a0f877c9ec998dbb943650b3b40b2f44a4a742de
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-27 11:12:57 +0530

    Navbar profile card added

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-    <h2 className='text-xl font-medium text-black py-2'>NoteMe</h2>  
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">NoteMe</h2>
+
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default Home
+export default Home;
