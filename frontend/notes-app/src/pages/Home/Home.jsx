5213cd0c978384838ab39363013a89b869f8600b
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-27 16:12:38 +0530

    Note Card Created

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-5 mt-8 mx-10">
+          <NoteCard
+            title="Meeting on 8th June"
+            date="8th June 2024"
+            content="Meeting on 8th June at SPW hotel, Kadawatha from 10am onwards."
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-14 h-14 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[28px] text-white" />
+      </button>
     </>
   );
 };
