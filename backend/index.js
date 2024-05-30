c9259860c4fd4157f7f0eec64278fc66154571ff
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-30 23:47:32 +0530

    Edit note API Created

--- backend/index.js
+++ backend/index.js
@@ -150,6 +150,44 @@ app.post("/add-note", authenticationToken, async (req, res) => {
   }
 });
 
+app.put("/edit-note/:noteId", authenticationToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body;
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ error: true, message: "No Changes provided" });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({
+      error: false,
+      note,
+      message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal Server Error",
+    });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
