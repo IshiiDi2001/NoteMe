faf1a7ea8d166f6ae3c99bd4b581bef61002a83b
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-05-30 19:42:03 +0530

    Login API Created

--- backend/index.js
+++ backend/index.js
@@ -75,6 +75,43 @@ app.post("/create-account", async (req, res) => {
   });
 });
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is require" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      message: "Login Succeeful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
+});
+
 app.listen(8000);
 
 module.exports = app;
