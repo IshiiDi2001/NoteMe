20076d8e5cc3fa5e93e8bfd64ac98dc74ff3f7c4
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-01 18:17:08 +0530

    Fixed some issues

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "./constants";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
-  headers: { "Content-Type": "application/json," },
+  headers: { "Content-Type": "application/json" },
 });
 
 axiosInstance.interceptors.request.use(
