9392d91a656a16993a128ce8e59fda83c2707880
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-01 14:22:52 +0530

    Get user API Created

--- backend/index.js
+++ backend/index.js
@@ -114,6 +114,27 @@ app.post("/login", async (req, res) => {
   }
 });
 
+// Get User
+app.get("/get-user", authenticationToken, async (req, res) => {
+  const { user } = req.user;
+
+  const isUser = await User.findOne({ _id: user._id });
+
+  if (!isUser) {
+    return res.sendStatus(401);
+  }
+
+  return res.json({
+    user: {
+      fullName: isUser.fullName,
+      email: isUser.email,
+      _id: isUser._id,
+      createdOn: isUser.createdOn,
+    },
+    message: "",
+  });
+});
+
 // Add Note
 app.post("/add-note", authenticationToken, async (req, res) => {
   const { title, content, tags } = req.body;
