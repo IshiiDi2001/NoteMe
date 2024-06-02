e0804f44efbc216624a248cade8a3e0aed81d59c
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-02 19:16:51 +0530

    Search Bar Removed from Login & Signup pages

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const noSearchBarRoutes = ["/login", "/signUp"];
 
   const onLogout = () => {
     localStorage.clear();
@@ -24,6 +26,8 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
     handleClearSearch();
   };
 
+  console.log("Current Path:", location.pathname);
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <div className="flex items-center gap-2">
@@ -31,14 +35,16 @@ const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
         <h2 className="text-xl font-medium text-black py-2">NoteMe</h2>
       </div>
 
-      <SearchBar
-        value={searchQuery}
-        onChange={({ target }) => {
-          setSearchQuery(target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch}
-      />
+      {!noSearchBarRoutes.includes(location.pathname) && (
+        <SearchBar
+          value={searchQuery}
+          onChange={({ target }) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
+      )}
 
       {userInfo && <ProfileInfo userInfo={userInfo} onLogout={onLogout} />}
     </div>
