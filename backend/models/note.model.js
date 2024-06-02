de300cfc4f69690af5571ffb8a6caba3dde40dae
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-02 21:19:09 +0530

    Note Model Added

---
+++ backend/models/note.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: { type: [String], default: [] },
+  isPinned: { type: Boolean, default: false },
+  userId: { type: String, required: true },
+  createOn: { type: Date, default: new Date().getTime() },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
